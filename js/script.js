3121407d51107fecb12f30cb7085f67530edf6c3
rosebud23x <112914733+rosebud23x@users.noreply.github.com>
2022-11-03 23:07:31 -0400

    code updates

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log("Hello world")
+
+/*Menu toggle*/
+function menuToggle(x) {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
+$('#click_advance').click(function() {
+    $('#display_advance').toggle('1000');
+    $("i", this).toggleClass("fas fa-bars fas fa-x");
+});
